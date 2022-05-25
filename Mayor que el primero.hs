165c54ade9b3a6349937e84205070dc758a813e0
tsepulvedf <85259415+tsepulvedf@users.noreply.github.com>
2022-05-25 02:15:22 -0500

    Update Mayor que el primero.hs

--- Mayor que el primero.hs
+++ Mayor que el primero.hs
@@ -3,8 +3,11 @@ import Data.List
 mayor :: [Int] -> Int -> [Int]
 mayor xs n = filter (> n) xs
 
+
 main = do
-  putStrLn "Los numeros mayores que el primero son: "
   let l1 = [5,7,8,2,3,4]
   let cabeza = head l1
+  if mayor l1 cabeza == []
+    then putStrLn "No hay ningun numero mayor que el primero"
+  else putStrLn "Los numeros mayores que el primero son: "
   print(mayor l1 cabeza)
